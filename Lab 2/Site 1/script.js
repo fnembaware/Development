732b48ce503f2523a6b180576093d99dc9733f2f
Fadzai Emmah <fadzai@tlu.ee>
2024-02-28 06:47:58 +0200

    Set timer to work

---
+++ Lab 2/Site 1/script.js
@@ -0,0 +1,27 @@
+let countDownDate = new Date("Oct 19,2024 00:00:00").getTime();
+let x = setInterval(function() {
+
+let now = new Date().getTime();
+let distance = countDownDate - now;
+
+let days = Math.floor(distance/(1000*60*60*24));
+let hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+let minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+let seconds = Math.floor((distance%(1000*60))/1000);
+
+document.getElementById("days").innerHTML = days < 10 ? "0" + days : days;
+document.getElementById("hours").innerHTML = hours < 10 ? "0" + hours : hours;
+document.getElementById("minutes").innerHTML = minutes < 10 ? "0" + minutes : minutes;
+document.getElementById("seconds").innerHTML = seconds < 10 ? "0" + seconds : seconds;
+
+if (distance<0) {
+    clearInterval(x);
+
+    document.getElementById("days").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+
+    document.getElementById("expiredMessage").innerHTML = "EXPIRED";   
+}
+},1000);
