8619044c22e97885690189a15916232ad3bc4f58
Fadzai Emmah <fadzai@tlu.ee>
2024-03-07 18:58:45 +0200

    Fixed js code

--- Lab 4/app1/Hello.js
+++ Lab 4/app1/Hello.js
@@ -1,8 +1,14 @@
 'use strict';
 
 class HelloWorld extends React.Component { 
-        constructor(props) { super(props); 
-        this.state = { personName:'World' }; 
-        11. 12. } } 
-    render() { return React.createElement('h1', null, 'Hello, ' + this.state.personName); 
-}
+        constructor(props) { 
+            super(props); 
+            this.state = { personName:'World' }; 
+        }
+
+    render() { 
+        return React.createElement('h1', null, 'Hello, ' + this.state.personName); 
+    }
+        }
+
+        ReactDOM.render(React.createElement(HelloWorld), document.getElementById('app'));
